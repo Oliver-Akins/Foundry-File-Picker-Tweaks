0120aaaefb2f8573f8acdcdc3f3c736898aa1dad
Oliver-Akins <Oliver.akins1999@gmail.com>
2024-11-17 14:32:37 -0700

    Update the render hook to use the custom permission

--- src/hooks/renderFilePicker.mjs
+++ src/hooks/renderFilePicker.mjs
@@ -3,11 +3,10 @@ Hooks.on(`renderFilePicker`, (fp, jq, rd) => {
 	// MARK: Folder Restrict
 	const restrictFolder = game.settings.get(fpt.id, `folderRestriction`);
 	if (restrictFolder && fp.activeSource === `data`) {
-		const restrictionBypass = game.settings.get(fpt.id, `folderRestrictionBypassRole`);
 		const folderTarget = game.settings.get(fpt.id, `goToFolder`);
 		if (
 			folderTarget !== ``
-			&& !game.user.hasRole(restrictionBypass)
+			&& !PermissionManager.can(fpt.id, `bypassFolderNavigationLock`)
 			&& !fp.result.target.startsWith(folderTarget)
 		) {
 			fp.browse(folderTarget);
